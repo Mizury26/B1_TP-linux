132c0bb50ff5e54445fe114c85f136012ee2d45b
Manon-Arc <arcasmanon47@gmail.com>
2023-01-09 16:18:22 +0100

    idcard ok

--- TP3/srv/idcard/idcard.sh
+++ TP3/srv/idcard/idcard.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 echo "Machine name : $(hostnamectl hostname)"
-echo "OS $(cat /etc/os-release | head -n 2) and kernel version is $(uname -r)"
+echo "OS $(cat /etc/os-release | head -n 1 | cut -d '"' -f2) and kernel version is $(uname -r)"
 echo "IP : $(ip a | grep enp0s8 | grep inet | tr -s ' ' | cut -d'/' -f1 | cut -d ' ' -f3)"
 echo "RAM : $(free -h | grep Mem | tr -s ' ' | cut -d ' ' -f4) memory available on $(free -h | grep Mem | tr -s ' ' | cut -d ' ' -f2) total memory"
-echo "Disque : $(df -h | grep ' /$' |tr -s ' ' | cut -d' ' -f4) space left"
-
-
+echo "Disk : $(df -h | grep ' /$' |tr -s ' ' | cut -d' ' -f4) space left"
 echo "Top 5 processes by RAM usage :"
 processes="$(ps -e -o cmd= --sort=-%mem | head -n5 | cut -d' ' -f1)"
 while read line
 do
 echo "- $line"
 done <<< "${processes}"
-
-
 echo "Listening ports :"
-ports=$(ss -lutnpr4H | tr -s ' ' | cut -d' ' -f1)" $(ss -lutnpr4H | tr -s ' ' | cut -d' ' -f5 | cut -d':' -f2) $(ss -lutnpr4H | tr -s ' ' | cut -d' ' -f7 | cut -d'"' -f2)"
-while read line
-do
-echo "-  $line"
-done <<< "${ports}"
+while read line ; do
 
+  port="$(echo $line |tr -s " " | cut -d' ' -f5 | cut -d":" -f2)"
+  service="$(echo $line | cut -d'"' -f2)"
+  protocole="$(echo $line | cut -d' ' -f1)"
+
+echo "- $port $protocole : $service"
+done <<< "$(ss -alnpH4)"
 curl https://cataas.com/cat --output cat 2> /dev/null
 cat_name='cat'
 cat_type="$(file cat | cut -d' ' -f2)"
@@ -37,6 +35,4 @@ else
 echo "Not good format"
 fi
 new_cat_file="cat${file_type}"
-cp "${cat_name}" "${new_cat_file}"
-rm "${cat_name}"
 echo "Here is your random cat : ${new_cat_file}"
